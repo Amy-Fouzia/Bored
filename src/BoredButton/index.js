86e7d217473b677f5a587566ad4a15b9d4d857e6
Tanvi Saini <ts4175@nyu.edu>
2023-10-10 16:58:28 -0400

    landing page

--- src/BoredButton/index.js
+++ src/BoredButton/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-//import './boredbutton.css'
+import './boredbutton.css'
 
 export default function BoredButton(){
     return(
         <>
-            <h1> hello </h1>
+        
+            <h1 className= "title"> Garden Party </h1>
+            <p className="descrip"> click to watch your garden come to life! </p>
+
+
         </>
     )
 }
