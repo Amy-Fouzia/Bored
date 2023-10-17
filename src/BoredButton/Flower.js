f19c0ca0fc79d1fabdb0eb6ce9e0bed081bc14f0
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-10-17 14:09:50 -0400

    new components

--- src/BoredButton/Flower.js
+++ src/BoredButton/Flower.js
@@ -1,15 +1,100 @@
 import React from 'react'
-
+import seedOne from '../assets/seedOne.png'
+import seedTwo from '../assets/seedTwo.png'
+import seedThree from '../assets/seedThree.png'
+import sproutOne from '../assets/sproutOne.png'
+import sproutTwo from '../assets/sproutTwo.png'
+import sproutThree from '../assets/sproutThree.png'
+import seedlingOne from '../assets/seedlingOne.png'
+import seedlingTwo from '../assets/seedlingTwo.png'
+import seedlingThree from '../assets/seedlingThree.png'
+import plantOne from '../assets/plantOne.png'
+import plantTwo from '../assets/plantTwo.png'
+import plantThree from '../assets/plantThree.png'
+import flowerOne from '../assets/flowerOne.png'
+import flowerTwo from '../assets/flowerTwo.png'
+import flowerThree from '../assets/flowerThree.png'
+import witherOne from '../assets/witherOne.png'
+import witherTwo from '../assets/witherTwo.png'
+import witherThree from '../assets/witherThree.png'
 
 export default function Flower (props) {
+    const{ water, type, setWater, setType } = props
 
-    const{flower, growFlower} = props
+    const [img, setImg] = React.useState();
 
+    React.useEffect(() => {
+        if (type === 1) {
+            if (water === 0) {
+                setImg(seedOne);
+            }  
+            else if (water === 1) {
+                setImg(sproutOne.png);
+            } 
+            else if (water === 2) {
+                setImg(seedlingOne);
+            } 
+            else if (water === 3) {
+                setImg(plantOne);
+            } 
+            else if (water === 4) {
+                setImg(flowerOne);
+            } 
+            else if (water === 5) {
+                setImg(witherOne);
+            } 
+        }
+    
+        else if (type === 2) {
+            if (water === 0) {
+                setImg(seedTwo);
+            }  
+            else if (water === 1) {
+                setImg(sproutTwo);
+            } 
+            else if (water === 2) {
+                setImg(seedlingTwo);
+            } 
+            else if (water === 3) {
+                setImg(plantTwo);
+            } 
+            else if (water === 4) {
+                setImg(flowerTwo);
+            } 
+            else if (water === 5) {
+                setImg(witherTwo);
+            } 
+        }
+    
+        else if (type === 3) {
+            if (water === 0) {
+                setImg(seedThree);
+            } 
+            else if (water === 1) {
+                setImg(sproutThree);
+            }
+            else if (water === 2) {
+                setImg(seedlingThree);
+            } 
+            else if (water === 3) {
+                setImg(plantThree);
+            } 
+            else if (water === 4) {
+                setImg(flowerThree);
+            } 
+            else if (water === 5) {
+                setImg(witherThree);
+            } 
+        }
+    }, [type, water]);
+
+    console.log(water);
+    console.log(type);
+    console.log(img);
 
     return(
         <div>
-             flower
-
+            <img src={ img }/>
         </div>
     );
 }
--- src/BoredButton/index.js
+++ src/BoredButton/index.js
@@ -1,34 +1,24 @@
-import {useState} from 'react'
+import React from 'react'
 import styles from './boredbutton.module.css'
-import Blue from '../assets/Blue.jpeg'
-import Blue2 from '../assets/Blue.jpeg'
 import Flower from './Flower'
-const flowers=[
-    {src:Blue, count:0},
-    {src:Blue2, count: 0}
-]
-
-//create a linear grid of flowers, each with its own src and count respective to its height
 
 export default function BoredButton(){
-    const [flowers, growFlower] = useState([]) //use state to grow the flower by incrementing count on flower 
-
-    const handleClick = () => {
-        growFlower()
-    }
+    const [waterOne, setWaterOne] = React.useState(0);
+    const [typeOne, setTypeOne] = React.useState(1);
 
     return(
         <>
             <h1 className={styles.title}> Garden Party </h1>
             <p className={styles.descrip}> click to watch your garden come to life! </p>
-
-            <button onClick={handleClick}> <Flower flowers={flowers}/> </button>
-            <div className={styles.gardenGrid}>  
-            {flowers.map((flower) => (
-                <Flower key={flower.id} flower={flower} growFlower={growFlower}/>
-            ))}
+            
+            <div>  
+                    <Flower 
+                        water={waterOne} 
+                        type={typeOne}
+                        setWater={setWaterOne}
+                        setType={setTypeOne}
+                    />
             </div>
-
         </>
     )
 }
